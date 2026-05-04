93f88513ace9818a977bc27a526e08a0579e1e72
Mikhail Spirin <mike144513@gmail.com>
2026-05-04 13:16:04 +0500

    add media adaptive

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,22 +1,23 @@
 import styles from "./Card.module.css";
 import arrowImg from "./../../img/icons/arrow.svg";
 
-const Card = ({title, img}) => {
+const Card = ({ title, img }) => {
   return (
     <>
-      <div className={styles.card}>
-        <a href="#" className={styles.card__link}></a>
-        <img src={img} alt="card01" className={styles.card__img} />
+      <a href="#" className={styles.card}>
+        <img src={img} alt={title} className={styles.card__img} />
+
         <div className={styles.card__body}>
           <div className={styles.card__text}>
             <div className={styles.card__title}>{title}</div>
             <div className={styles.card__muted}>Explore Now!</div>
           </div>
+
           <div className={styles.card__icon}>
-            <img src={arrowImg} alt="arrow" className="card__icon" />
+            <img src={arrowImg} alt="" />
           </div>
         </div>
-      </div>
+      </a>
     </>
   );
 };
